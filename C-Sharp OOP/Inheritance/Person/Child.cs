28150f18cfca5c30f42fb4df4282051b88d75bfe
Krasimira Arizanova <70700274+KrasyAri@users.noreply.github.com>
2020-10-30 16:53:17 +0200

    files

---
+++ C-Sharp OOP/Inheritance/Person/Child.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonProblem
+{
+    public class Child : Person
+    {
+
+        public Child(string name, int age) : base(name, age)
+        {
+                
+        }
+    }
+}
---
+++ C-Sharp OOP/Inheritance/StackofStrings/CustomStack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+
+            stack.Push("Pesho");
+            stack.Push("Gosho");
+            stack.Push("Sasho");
+
+            Console.WriteLine(stack.IsEmpty());
+            
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
